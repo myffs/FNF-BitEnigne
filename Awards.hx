d336782901f897ab006231bfece818c4da6955a1
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2022-09-27 15:27:21 -0400

    Update Awards.hx

--- Awards.hx
+++ Awards.hx
@@ -1,5 +1,6 @@
+
 ["Very Bitty!", "beat all songs on easy", "10xp"],
 ["Fucking cheater!", "play songs on hard,then switch to easy", "10xp"],
 ["Funky night!", "get 100+ sicks on the difficultly HARD", "25xp"],
 ["No more dating?", "lose 10 rap battles" "2xp"],
-["fuck", "die in tutorial 1,000 times"]
+["fuck", "die in tutorial 1,000 times", "1000xp"]
