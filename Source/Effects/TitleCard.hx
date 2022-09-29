b690dbd5695819b7de2610fce667390d879f86e8
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2022-09-29 18:28:43 -0400

    i forgort about this shit.
    i added a title card in case someone makes a fucking pull request about it

---
+++ Source/Effects/TitleCard.hx
@@ -0,0 +1,13 @@
+class card {
+ ["Song Title"],
+   ["Credits,and Discription"],
+     ["Width + Height"]
+      /*when adding a song title card,
+remove the text and add your song title,credits,discription,width,and height*/
+/*if your adding more than one songs,add a "," to the end of the bracket,
+and add "class card2 {
+         ["Song Title"],
+            ["Credits,and Discription"],
+           ["Width + Height"]
+          }"*/
+}
