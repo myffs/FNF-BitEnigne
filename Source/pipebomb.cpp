d3349048c8b0dee0431f472834bcd2c8b12d54d4
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-10-01 10:00:22 -0400

    Update pipebomb.cpp

--- Source/pipebomb.cpp
+++ Source/pipebomb.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main()
 {
     char url[100] = "https://www.youtube.com/watch?v=G31AUfWMtZw";
-    ShellExecute(NULL, "open", url, NULL, NULL, SW_SHOWNORMAL);
+    ShellExecute(NULL, "open", url, NULL, NULL, SW_SHOWNORMAL); // open the url to youtube.com
     return 0;
 }
