78a786021c3b6ce96729db102a55bd67e45b8a01
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2022-10-08 17:25:46 -0400

    bad code SKULL

---
+++ Source/Stages/StageState.hx
@@ -0,0 +1,31 @@
+package;
+
+import.readme.md
+import.writing.hx
+import.cube.hx
+
+class { 
+           {
+  
+  {
+        {
+      {
+ {
+          
+  "idk how to code :sob:"
+   }
+} 
+  }
+    }
+     "cry about it"
+   }
+        }
+}
+  end
+    { stages : [
+    ["Stages"],
+      ["NONE ADDED"],
+    ["Custom Stages"],
+      ["NONE ADDED"]
+       ]
+    }
