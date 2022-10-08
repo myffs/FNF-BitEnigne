e368d9962db4606dbe9dfe1b6f6edcfd0b7fc790
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2022-10-08 19:29:43 -0400

    i kinda get it now
    i kinda get haxe a lil
    
    don't call me a baby,i'm just bad at coding

--- Source/Dialouge/DialogueState.hx
+++ Source/Dialouge/DialogueState.hx
@@ -3,13 +3,3 @@ package;
 class DialougeState {
   var dialouge1:Array<String> = ["Swaggy","P"];
 }
-//adding the dialouge
-
-/*if#
-pressing?('P')
-add(newtext)
-else#
-  class dialouge{
-    ["Swaggy","P"]
-  }
-*/
