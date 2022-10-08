e4dccf717d85e92580ba1ed852ccc04062e64555
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2022-10-08 19:46:48 -0400

    manage the achievements

--- Source/awards.hx
+++ Source/awards.hx
@@ -1,11 +1,15 @@
-
-["Awards"],
-[""],
-["Very Bitty!", "beat all songs on easy", "10xp"],
-["Fucking cheater!", "play songs on hard,then switch to easy", "10xp"],
-["Funky night!", "get 100+ sicks on the difficultly HARD", "25xp"],
-["No more dating?", "lose 10 rap battles" "2xp"],
-["fuck", "die in tutorial 1,000 times", "21xp"],
-["rapping star!", "beat a song on the scroll speed of 10", "57xp"],
-["Your so funny UwU", "spam chart and arrow keys in tutorial", "65xp"],
-[""]
+["Beginning"],
+  ['5xP']
+  ['10xP'],
+["Advanced"],
+  ['15xP'],
+  ['20xP'],
+["Expert"],
+  ['30xP'],
+  ['40xP'],
+  ['50xP'],
+  ['60xP'],
+  ['70xP'],
+  ['80xP'],
+  ['90xP'],
+  ['100xP']
