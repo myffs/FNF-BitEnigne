e18f9d5a008b0c51169e0ffeec76fe25f63772d8
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2022-10-08 22:56:55 -0400

    achievements

---
+++ Source/AchievementState.hx
@@ -0,0 +1,13 @@
+package;
+
+import.awards.hx
+
+class achievementstate {
+  var achievements:array<string> = ["Just the beginning","complete week 1 with no misses"],
+                                   ["Shitted my ass","complete week 2 with no misses"],
+                                   ["SHOT GUN!","die 5 times on philly"],
+                                   ["Hot like the sun","complete week 4 with no misses"],
+                                   ["crispy-cream lemon please?","complete week 5 with no misses"],
+                                   ["2-bit world","die 6,000 times in thorns"];
+  }
+    
