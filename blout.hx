e780216de8a7d8e9e8c6f05adbc9fb396eea9671
HAZZZZZZZE <115948389+MMbay64@users.noreply.github.com>
2022-10-25 16:56:26 -0400

    Compile code?

---
+++ blout.hx
@@ -0,0 +1,16 @@
+6R#A
+$ load prodotype
+
+$ function
+ start up
+import
+'introtext.text'
+$ pick()
+> 1--2
+> 3--4
+$ function
+export
+blout.hx
+$ compile('1500-400-100_47_build_1299)
+
+//Compile code?
