60869ae722583534aa3573caebb76967d2d47dbc
HAZZZZZZZE <115948389+MMbay64@users.noreply.github.com>
2022-10-26 16:00:45 -0400

    I kinda added it

---
+++ Source/Time bar.hx
@@ -0,0 +1,13 @@
+package;
+
+class timestate {
+var timebar:array<string> = [""],
+var timeUI:array<string> = ["0:00"],
+var timeUItick1:array<string> = ["0:01"],
+var timeUItick2:array<string> = ["1:00"],
+var timeUItick3:array<string> = ["0:10"],
+var timebarcol:array<string> = ["#000000"],
+var timeUI:array<string> = ["#FFFFFF"],
+var timeUIfont:array<string> =["assets/fonts/PIXEL.tiff"], 
+var timeUIfontoutline:array<string> = ["0.0003"],
+var timeUIfontoutlinecol:array<string> = ["#000000"]
