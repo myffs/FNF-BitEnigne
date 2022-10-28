aa27e25f6054ca2a041f53abf2402a4e02667053
MMbay64 <115948389+MMbay64@users.noreply.github.com>
2022-10-28 19:19:16 -0400

    All the new, updated chart events
    Add this?

---
+++ ChartEvents/ChartEvents.hx
@@ -0,0 +1,17 @@
+package;
+
+ class modchartstate { 
+["ModChart"],
+['this will effect the chart to a modded states,1 is on, 2 is off],
+['modchart1 is Dave and Bambi ModChart, anything else is random']
+},
+ class healthdrainstate {
+["HealthDrain"],
+['this will drain your health bar when the opponent sings,1 is on,2 is off'],
+['0.02 is default']
+},
+ class vanillachartstate {
+["VanillaChart"],
+['this will swing the arrows side to side,1 is on,2 is off'],
+['value2 is the acceleration,0.04 is default']
+},
