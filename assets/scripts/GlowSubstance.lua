c1b97b60d29a14715555874c98bf54aa300e6e92
MMbay64 <115948389+MMbay64@users.noreply.github.com>
2022-10-29 06:19:54 -0400

    Scripts

---
+++ assets/scripts/GlowSubstance.lua
@@ -0,0 +1,9 @@
+{
+  SetProporty( 100 * 2 > 12 * 3 )
+   SetProporty( 100 < 3 ) 
+    GetProporty(y)
+   GetProporty(x)
+     SubstanceX( 0.00005 = x )
+   SubstanceY( 0.00007 = y )
+  ResetSubstance( 50 x 50 )
+}
