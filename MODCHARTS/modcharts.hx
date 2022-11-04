a1ccd03f9c8e9afad099bb1189b648ee73e179e7
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2022-11-04 18:28:29 -0400

    hehe moddy

---
+++ MODCHARTS/modcharts.hx
@@ -0,0 +1,15 @@
+package;
+
+class modcharts [
+public static var modchartstuff:array<dynamic> - [ //first add your modchart name,then add the import
+["Moving Arrows",   "makes the arrows move",   "File Name:MovingArrows.lua"],
+["Bouncy Icons",    "Bounces the icons like dave and bambi",   "File Name:BouncyIcons.lua"],
+["Bouncy Golden Apple Icons",   "Bounces the icons like dave and bambi golden apple edtion",   "File Name:BouncyIconsDaveAndBambiGoldeApple.lua"]
+
+];
+
+class define {
+find(BouncyIconsDaveAndBambiGoldeApple.lua)
+find(BouncyIcons.lua)
+find(MovingArrows.lua)
+}
