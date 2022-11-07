5f2f8ce3272eb79f1a14733bafc408bb2e458d59
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2022-11-07 15:32:28 -0500

    added custom ones

--- MODCHARTS/modcharts.hx
+++ MODCHARTS/modcharts.hx
@@ -4,7 +4,8 @@ class modcharts [
 public static var modchartstuff:array<dynamic> - [ //first add your modchart name,then add the import
 ["Moving Arrows",   "makes the arrows move",   "File Name:MovingArrows.lua"],
 ["Bouncy Icons",    "Bounces the icons like dave and bambi",   "File Name:BouncyIcons.lua"],
-["Bouncy Golden Apple Icons",   "Bounces the icons like dave and bambi golden apple edtion",   "File Name:BouncyIconsDaveAndBambiGoldeApple.lua"]
+["Bouncy Golden Apple Icons",   "Bounces the icons like dave and bambi golden apple edtion",   "File Name:BouncyIconsDaveAndBambiGoldeApple.lua"],
+["Custom",   "Add discription here" "File Name:FILENAME.lua"],//note; the file MUST be in lua or else your a retard...and don't follow rules....
 
 ];
 
@@ -12,4 +13,5 @@ class define {
 find(BouncyIconsDaveAndBambiGoldeApple.lua)
 find(BouncyIcons.lua)
 find(MovingArrows.lua)
+find(FILENAME.lua)//replace the "FILENAME" to your custom file name
 }
