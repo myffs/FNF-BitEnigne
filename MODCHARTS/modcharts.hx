fd22c50bed0150ef58d18eff8ec54bed3c9bd1dc
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2022-12-17 09:37:30 -0500

    ..

--- MODCHARTS/modcharts.hx
+++ MODCHARTS/modcharts.hx
@@ -5,6 +5,7 @@ public static var modchartstuff:array<dynamic> - [ //first add your modchart nam
 ["Moving Arrows",   "makes the arrows move",   "File Name:MovingArrows.lua"],
 ["Bouncy Icons",    "Bounces the icons like dave and bambi",   "File Name:BouncyIcons.lua"],
 ["Bouncy Golden Apple Icons",   "Bounces the icons like dave and bambi golden apple edtion",   "File Name:BouncyIconsDaveAndBambiGoldeApple.lua"],
+["Karen Ass Like",   "Fucking joke dumbass",   "File Name:karen.lua"],
 ["Custom",   "Add discription here", "File Name:FILENAME.lua"],//note; the file MUST be in lua or else your a retard...and don't follow rules....
 
 ],
@@ -13,5 +14,6 @@ class define {
 find(BouncyIconsDaveAndBambiGoldeApple.lua);
 find(BouncyIcons.lua);
 find(MovingArrows.lua);
+find(karen.lua);
 find(FILENAME.lua)//replace the "FILENAME" to your custom file name
 }
