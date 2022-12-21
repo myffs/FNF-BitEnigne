7fd3e239e72587e813c7a6b45394d9f7b6e05786
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2022-12-21 12:42:31 -0500

    some pressed files

---
+++ compressed/depresser.cpp
@@ -0,0 +1,8 @@
+class reset {
+    trace('outdated,go to echo engine to find new updates','enter','backspace')
+}
+ifkeypressed('enter');
+  fetch('https://github.com/LilDrippyMyFnf/Fnf-EchoEngine/releases/tag/0.1.0');
+ifkeypressed('backspace');
+  fetch('storymodemenu');
+//fetching shit
